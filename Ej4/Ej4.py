30760c8f569468898b985aa2018cbafe92240afb
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2021-11-04 12:23:22 +0100

    añado comentarios al ej4

--- Ej4/Ej4.py
+++ Ej4/Ej4.py
@@ -1,7 +1,9 @@
 # 4- Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario devuelve False.
 char = input('Introduce un caracter: ')
-vocales ="aeiou"
+vocales ="aeiou" 
+#declaro una variable tipo cadena que incluya todas las vocales
 def miCar(a):
+#declaro una función para buscar el caracter pasado por teclado en la cadena de vocales, uso la funcion string.find("valorABuscar"), si devuelve -1 es que no la ha encontrado y es una consonante
     if (vocales.find(a)<0):
         b='False, es una consonante'
     else:
