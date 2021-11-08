9af366b4b350f9008f30c1fb64c2097cd8122adf
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2021-11-08 14:40:02 +0100

    modifico 5 pero imcompleto

--- Ej5/Ej5.py
+++ Ej5/Ej5.py
@@ -1,5 +1,18 @@
 # 5- Escribir una función sum() y una función multip() que sumen y multipliquen respectivamente todos los números de una lista. Por ejemplo: sum([1,2,3,4]) debería devolver 10 y multip([1,2,3,4]) debería devolver 24.
 
+def sum(l):
+    resultado=0 #acumulador
+    for i in range(longitud):
+        resultado=lista[i]+lista[i+1]
+    return resultado
+
+def multip():
+    resultado=1 #acumulador 
+    for i in range(longitud):
+        resultado=lista[i]*lista[i+1]
+    return resultado
+
+
 longitud = int(input('Introduce un número de elementos que va a tener la lista: '))-1
 lista=[] #me obliga a inicializar la lista
 #print(longitud)
@@ -10,19 +23,9 @@ for i in range(longitud):
 
 opcion = int(input('Para usar la funcion de sumar introduzca 1 para multiplicar 2, para salir cualquier otro valor: '))
 if opcion==1:
-
-    def sum(l):
-        i=0
-        for i in range(longitud):
-            resultado=lista[i]+lista[i+1]
-        return resultado
     print('La sumatoria de los elementos de la lista es: ',sum(lista))
+
 if opcion==2:
-    def multip():
-        i=1
-        for i in range(longitud-1):
-            resultado=lista[i]*lista[i+1]
-        return resultado
-        print('La multiplicacion de los elementos de la lista es: ',resultado)
+    print('La multiplicacion de los elementos de la lista es: ',resultado)
 else:
     print('Hasta luego!')
